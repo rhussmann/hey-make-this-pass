aca45dda0a3c9946898772e22512f7715349f965
David A. Riggs <driggs@myotisoft.com>
2018-10-13 12:12:57 -0400

    add some heavy duty math functions, not quite correct yet...

--- src/hey_make_this_pass/core.clj
+++ src/hey_make_this_pass/core.clj
@@ -4,3 +4,13 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn even
+  "Is this number even?"
+  [n]
+  (= (mod n 3) 0))
+
+(defn odd
+  "Is this number odd?"
+  [n]
+  (not (even n)))
--- test/hey_make_this_pass/core_test.clj
+++ test/hey_make_this_pass/core_test.clj
@@ -5,3 +5,11 @@
 (deftest a-test
   (testing "I pass!"
     (is (= 1 1))))
+
+(deftest even-test
+  (testing "Help, I fail!"
+    (is (even 100))))
+
+(deftest odd-test
+  (testing "Help, I fail!"
+    (is (odd 99))))
